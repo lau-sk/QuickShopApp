7c01a2ad311cf695c3e5d0f0aaa1d3beaa9b2078
lau-sk <laurenashleyblack030@gmail.com>
2023-05-16 22:17:23 -0500

    Minor code clean up

--- src/screens/signInScreen.js
+++ src/screens/signInScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Button, View, Text, Alert} from 'react-native';
+import {Button, View, Text} from 'react-native';
 import {SignUpScreen} from './signUpScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -8,7 +8,7 @@ export function SignInScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Sign In Screen</Text>
-      <Button title="Sign up" onPress={() => navigation.navigate('SignIn')} />
+      <Button title="Sign up" onPress={() => navigation.navigate('SignUp')} />
     </View>
   );
 }
